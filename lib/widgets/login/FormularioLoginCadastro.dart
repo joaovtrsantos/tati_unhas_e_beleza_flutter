db90fc77772cb83bb8ccc8ad90867d7c4d8d2a4e
joaovtrsantos <dev.joaovitor1@gmail.com>
2025-05-08 20:56:38 -0300

    feat: Cadastro de informações adicionais no cadastro do usuário.

--- lib/widgets/login/FormularioLoginCadastro.dart
+++ lib/widgets/login/FormularioLoginCadastro.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:tati_unhas_e_beleza_flutter/widgets/PaginaInicial.dart';
@@ -8,22 +9,28 @@ import 'package:tati_unhas_e_beleza_flutter/widgets/login/InputFormulario.dart';
 import 'package:tati_unhas_e_beleza_flutter/widgets/login/PaginaLoginCadastro.dart';
 
 class FormularioLoginCadastro extends StatelessWidget {
-  const FormularioLoginCadastro({this.isLogin = true, required this.onAlternarModo, super.key});
+  const FormularioLoginCadastro({
+    this.isLogin = true,
+    required this.onAlternarModo,
+    super.key,
+  });
 
   final bool isLogin;
   final VoidCallback onAlternarModo;
-  
+
   @override
   Widget build(BuildContext context) {
     final TextEditingController _controllerEmail = TextEditingController();
-    final TextEditingController _controllerPassword = TextEditingController();
+    final TextEditingController _controllerSenha = TextEditingController();
+    final TextEditingController _controllerNome = TextEditingController();
+    final TextEditingController _controllerTelefone = TextEditingController();
     String? errorMessage = '';
-    
-  Future<void> signInWithEmailAndPassword() async {
+
+    Future<void> signInWithEmailAndPassword() async {
       try {
         await Auth().signInWithEmailAndPassword(
           email: _controllerEmail.text,
-          password: _controllerPassword.text,
+          password: _controllerSenha.text,
         );
       } on FirebaseAuthException catch (e) {
         errorMessage = e.message;
@@ -34,13 +41,20 @@ class FormularioLoginCadastro extends StatelessWidget {
       try {
         await Auth().createUserWithEmailAndPassword(
           email: _controllerEmail.text,
-          password: _controllerPassword.text,
+          password: _controllerSenha.text,
         );
+
+        CollectionReference usuarios = FirebaseFirestore.instance.collection('Usuarios');
+        await usuarios.add({
+          'admin': false,
+          'email': _controllerEmail.text,
+          'nome': _controllerNome.text,
+          'telefone': _controllerTelefone.text,
+        });
       } on FirebaseAuthException catch (e) {
         errorMessage = e.message;
       }
     }
-
 
     return Form(
       child: Column(
@@ -51,27 +65,43 @@ class FormularioLoginCadastro extends StatelessWidget {
           InputFormulario(_controllerEmail),
           const SizedBox(height: 20),
           const Text('Senha', textAlign: TextAlign.left),
-          InputFormulario(_controllerPassword),
+          InputFormulario(_controllerSenha),
+          if (!isLogin) ...[
+            SizedBox(height: 20),
+            const Text('Nome', textAlign: TextAlign.left),
+            InputFormulario(_controllerNome),
+            SizedBox(height: 20),
+            const Text('Telefone', textAlign: TextAlign.left),
+            InputFormulario(_controllerTelefone),
+          ],
           const SizedBox(height: 30),
           BotaoSubmit(
-           onPressed: () async {
-            if(isLogin) {
-              await signInWithEmailAndPassword();
-              if (context.mounted){
-                Navigator.push(context, MaterialPageRoute(builder: (_) =>  TatiUnhasEBelezaApp()));
+            onPressed: () async {
+              if (isLogin) {
+                await signInWithEmailAndPassword();
+                if (context.mounted) {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (_) => TatiUnhasEBelezaApp()),
+                  );
+                }
+              } else {
+                createUserWithEmailAndPassword();
+                if (context.mounted) {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (_) => LoginCadastro()),
+                  );
+                }
               }
-            } else {
-              createUserWithEmailAndPassword();
-              if (context.mounted){
-                Navigator.push(context, MaterialPageRoute(builder: (_) => LoginCadastro()));
-              }
-            }
-           },
-           texto: isLogin ? "Entrar" : "Cadastrar",
+            },
+            texto: isLogin ? "Entrar" : "Cadastrar",
           ),
           Text(errorMessage == '' ? '' : 'Erro ao fazer login $errorMessage'),
-          isLogin ? BotaoCadastro(onPressed: onAlternarModo) : BotaoCadastro(isLogin: false, onPressed: onAlternarModo)
-        ]
+          isLogin
+              ? BotaoCadastro(onPressed: onAlternarModo)
+              : BotaoCadastro(isLogin: false, onPressed: onAlternarModo),
+        ],
       ),
     );
   }
